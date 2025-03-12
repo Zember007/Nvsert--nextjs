dbec4f96f8b21ac977c90d0346158c60ad5f1e68
Zember007 <goraegorgora@mail.ru>
2025-03-12 13:13:36 +0400

    commit

--- src/components/main/AppMainDocuments.tsx
+++ src/components/main/AppMainDocuments.tsx
@@ -1,7 +1,6 @@
 import MainDocumentItem from "./elements/MainDocumentItem";
 import { documents } from "./utils";
 import { useState } from "react";
-import '@/assets/styles/sections/main/animation/documents.scss'
 
 
 
@@ -10,6 +9,7 @@ import '@/assets/styles/sections/main/animation/documents.scss'
 const AppMainDocuments = () => {
 
     const [activeIndex, setActive] = useState<number | null>(null)
+    const [hoverIndex, setHover] = useState<number | null>(null)
     return (
         <section className="py-[75px] flex flex-col gap-[50px]">
             <div className="wrapper">
@@ -17,13 +17,18 @@ const AppMainDocuments = () => {
                 <h2 className="leading-[1] text-center l:text-left text-[24px] xs:text-[40px] l:text-[56px] text-[#000000] tracking-[-0.04em]">Мы оформляем следующие документы</h2>
 
             </div>
-            <div className="flex flex-col items">
+            <div className="flex flex-col">
                 {
                     documents.map((item, index) =>
-                        <MainDocumentItem setActive={() => setActive(index)} active={index === activeIndex} key={index} {...item} />
+
+                        <MainDocumentItem
+                            bordert={(index - 1 !== hoverIndex && index - 1 !== activeIndex)}
+                            borderb={index + 1 !== hoverIndex || activeIndex===hoverIndex}
+                            setHover={(value) => { setHover(value ? index : null) }} setActive={(value) => setActive(value ? index : null)} active={index === activeIndex} key={index} {...item} />
+
                     )
                 }
-                
+
             </div>
         </section>
     );
--- src/components/main/elements/MainDocumentItem.tsx
+++ src/components/main/elements/MainDocumentItem.tsx
@@ -1,7 +1,7 @@
 import ArrowImg from '@/assets/images/svg/arrow-main.svg'
 import Image from 'next/image';
 import { useState } from 'react';
-import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+import { StaticImageData, StaticImport } from 'next/dist/shared/lib/get-img-props';
 
 interface content {
     text: string,
@@ -14,90 +14,124 @@ interface content1 {
 }
 
 interface props {
-    img: string | StaticImport,
+    img:  StaticImageData,
     title: string,
     content: content,
     content1: content1[],
-    active:boolean, 
-    setActive:() => void
+    active: boolean,
+    setActive: (value: boolean) => void,
+    setHover: (value: boolean) => void,
+    bordert: boolean,
+    borderb: boolean
 }
 
-const MainDocumentItem = ({ img, title, content, content1, active, setActive }: props) => {
+const MainDocumentItem = ({ img, title, content, content1, active, setActive, borderb, bordert, setHover }: props) => {
 
     const [listHidden, setListHidden] = useState(true)
+
     return (
-        <div tabIndex={0}
-        onClick={() => {
-            setActive()
-        }}
-        className={`${active && 'active'} item group  cursor-pointer`}>
-            <div className="wrapper !px-[50px]">
-                <div className=" item-border flex flex-col">
+        <div
+            onMouseEnter={() => { setHover(true) }}
+            onMouseLeave={() => { setHover(false) }}
+            className={` ${!active && 'hover:shadow-[0px_2px_4px_0px_#00000040_inset,_0px_-2px_4px_0px_#00000040_inset] hover:bg-[#FFF]'}  transition-all duration-300 cursor-pointer`}>
+            <div className="  flex flex-col">
+                <div className="wrapper">
                     <div
-                      
-                        className="flex items-center justify-between py-[15px] s:py-[23px]">
-                        <p className="text-[16px] s:text-[18px] m:text-[20px] text-[#000] font-bold tracking-normal align-middle leading-tight">{title}</p>
-                        <Image className={`${!active && '!rotate-[180deg]'} transition-all duration-300`} alt='arrow' src={ArrowImg} width={24} height={24} />
-                    </div>
-                    <div className={` flex flex-col l:flex-row justify-between m:items-stretch gap-[10px] transition-all duration-300 overflow-hidden max-h-0 group-focus:!max-h-[1200px] group-focus:s:pb-[23px] group-focus:pb-[15px] ${active && '!max-h-[1200px] s:pb-[23px] pb-[15px]'}`}>
-                        <div className="s:gap-[40px] gap-[20px] justify-between flex flex-col m:flex-row m:items-stretch">
-                            <div className='m:m-0 m-auto'>
-                                {img && <Image alt='document' src={img} />}
-                            </div>
-
-                            <div className=" flex flex-col justify-between  items-start">
-                                <div className="flex flex-col gap-[40px]">
-                                    <p className='text-[16px] text-[#000000] m:max-w-[360px]'>
-                                        {content.text}
-                                    </p>
-                                    <p className='text-[16px] text-[#000000] m:max-w-[300px]'>
-                                        {content.text1}
-                                    </p>
-                                </div>
-                                <button className='m:block hidden px-[30px] py-[18px] text-[20px] font-bold rounded-[4px] bg-[#000000] leading-[1] text-[#FFF]'>
-                                    Оформить заявку
-                                </button>
-                            </div>
+                        onClick={() => {
+                            setActive(true)
+                        }}
+                        className={`flex items-center border-0   border-solid border-[#00000033] ${!bordert && 'border-t-[transparent]'} border-t-[0.5px] ${(!borderb || active) && 'border-b-[transparent]'} border-b-[0.5px]  justify-between py-[15px] s:py-[23px] ${active && 'text-[#34446D]'} hover:text-[#34446D] text-[#000] transition-all duration-300 relative ${!active && 'pl-[63px] hover:border-[transparent] '}`}>
+                        <div className={`transition-all duration-300 absolute top-1/2 left-0 translate-y-[-50%] ${active && 'translate-y-[60px]'}`}>
+                            <Image alt='document' src={img}
+                                width="0"
+                                height="0"
+                                sizes="100vw"
+                                className={`transition-all duration-300 ${!active && 'w-[43px]'} h-auto`} />
                         </div>
-
-                        <div className="flex gap-[10px] flex-col  m:max-w-[500px] text-[#000]">
-
-                            {
-                                content1.map((cont, contIndex) => (
-                                    <div key={contIndex} className='flex gap-[10px] flex-col items-start'>
-                                        <p className='text-[20px] font-bold'>{cont.title}</p>
-
-                                        <ul className=' list-disc pl-[20px] flex flex-col gap-[6px]'>
-
-                                            {
-                                                cont.list.map((list, index) => (
-                                                    <li className={`${listHidden && index > 4 && 'hidden'}`} key={index}>{list}</li>
-                                                ))
-                                            }
-
-                                        </ul>
-
-
-                                        {
-                                            listHidden && <button
-                                                onClick={() => setListHidden(false)}
-                                            >Показать полный список документов 🡣</button>
-                                        }
-                                    </div>
-                                ))
-                            }
-
+                        <p className="w-1/2 text-[16px] s:text-[18px] m:text-[20px]  font-bold tracking-normal">{title}</p>
+                        <div className="w-1/2 flex items-center justify-between">
+                            <p className="text-[16px] s:text-[18px] m:text-[20px]  font-bold tracking-normal">от 2-х дней</p>
+                            <p className="text-[16px] s:text-[18px] m:text-[20px]  font-bold tracking-normal">от 6 800 ₽</p>
+                            <button
+                                onClick={() => {
+                                    setTimeout(() => {
+                                        if (!active) return
+                                        setActive(false)
+                                    }, 100)
+                                }}>
+                                <Image
+                                    className={`${!active && 'rotate-[180deg]'} transition-all duration-700`} alt='arrow' src={ArrowImg} width={24} height={24} />
+                            </button>
                         </div>
-
-                        <button className='m:hidden  py-[18px] text-[20px] font-bold rounded-[4px] bg-[#000000] leading-[1] text-[#FFF]'>
-                                    Оформить заявку
-                                </button>
-
                     </div>
                 </div>
+                <div className={`${active && 'bg-[#FFF] shadow-[0px_2px_4px_0px_#00000040_inset,_0px_-2px_4px_0px_#00000040_inset]'}`}>
+                    <div className="wrapper">
+                        <div className={` flex flex-col l:flex-row justify-between m:items-stretch gap-[10px] transition-all easy-in duration-500 overflow-hidden max-h-0 ${active && '!duration-700 !max-h-[1200px] s:py-[23px] py-[15px]'}`}>
+                            <div className="s:gap-[40px] gap-[20px] justify-between flex flex-col m:flex-row m:items-stretch">
+                                <div className='m:m-0 m-auto'>
+                                    <div 
+                                    style={{
+                                        width: img.width + 'px',
+                                        height: img.height + 'px'
+                                    }}
+                                    ></div>
+                                </div>
 
+                                <div className=" flex flex-col justify-between  items-start">
+                                    <div className="flex flex-col gap-[40px]">
+                                        <p className='text-[16px] text-[#000000] m:max-w-[360px]'>
+                                            {content.text}
+                                        </p>
+                                        <p className='text-[16px] text-[#000000] m:max-w-[300px]'>
+                                            {content.text1}
+                                        </p>
+                                    </div>
+                                    <button className='text-[20px]  font-bold tracking-normal m:block hidden px-[30px] py-[14px] text-[#34446D] rounded-[4px] bg-[#2D2F2F1A] border-[#34446D] border border-solid leading-[1] text-[#FFF]'>
+                                        Оформить заявку
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className="flex gap-[10px] flex-col  m:max-w-[500px] text-[#000]">
+
+                                {
+                                    content1.map((cont, contIndex) => (
+                                        <div key={contIndex} className='flex gap-[10px] flex-col items-start'>
+                                            <p className='text-[20px] font-bold'>{cont.title}</p>
+
+                                            <ul className=' list-disc pl-[20px] flex flex-col gap-[6px]'>
+
+                                                {
+                                                    cont.list.map((list, index) => (
+                                                        <li className={`${listHidden && index > 4 && 'hidden'}`} key={index}>{list}</li>
+                                                    ))
+                                                }
+
+                                            </ul>
+
+
+                                            {
+                                                listHidden && cont.list.length > 5 && <button
+                                                className='text-[#34446D] font-bold'
+                                                    onClick={() => setListHidden(false)}
+                                                >Показать полный список документов 🡣</button>
+                                            }
+                                        </div>
+                                    ))
+                                }
+
+                            </div>
+
+                            <button className='m:hidden  py-[18px] text-[20px] font-bold rounded-[4px] bg-[#000000] leading-[1] text-[#FFF]'>
+                                Оформить заявку
+                            </button>
+
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </div >
     );
 };
 
