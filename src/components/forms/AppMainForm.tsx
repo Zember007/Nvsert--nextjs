c3a8dc1fc095353ef40aa56ca6f12f0b1b7e7d2a
Zember007 <goraegorgora@mail.ru>
2025-03-18 14:12:28 +0400

    commot

--- src/components/forms/AppMainForm.tsx
+++ src/components/forms/AppMainForm.tsx
@@ -8,13 +8,14 @@ import axios from "axios";
 import { useHeaderContext } from "../contexts/HeaderContext";
 import MessageImg from '@/assets/images/svg/message-flight.svg'
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
-const AppMainForm = ({ btnText }: { btnText:string }) => {
+const AppMainForm = ({ btnText }: { btnText: string }) => {
 
     const { t } = useTranslation()
     const { openDefaultModal } = useHeaderContext()
 
-    const onSubmit = async(e:any) => {
+    const onSubmit = async (e: any) => {
         const formData = new FormData();
         for (const key in e) {
             if (e.hasOwnProperty(key)) {
@@ -37,6 +38,64 @@ const AppMainForm = ({ btnText }: { btnText:string }) => {
 
     const methods = useForm({ mode: "onTouched" });
     const { reset } = methods
+
+    const buttonRefs = useRef<HTMLButtonElement[]>([]);
+    const wrapperRefs = useRef<HTMLDivElement[]>([]);
+
+    const setWrapperRef = (el: HTMLDivElement | null) => {
+        if (!el) return
+        wrapperRefs.current.push(el)
+    }
+
+    const setButtonRef = (el: HTMLButtonElement | null) => {
+        if (!el) return
+        buttonRefs.current.push(el)
+    }
+
+    useEffect(() => {
+        const buttons = buttonRefs.current;
+        const wrappers = wrapperRefs.current;
+
+        if (!buttons.length || !wrappers.length) return;
+
+        const handleMouseMove = (e: MouseEvent, element: HTMLElement) => {
+            const rect = element.getBoundingClientRect();
+            const mouseX = e.clientX - rect.left;
+            const mouseY = e.clientY - rect.top;
+            const rotateX = (mouseY / rect.height) * 30 - 15;
+            const rotateY = (mouseX / rect.width) * -30 + 15;
+            element.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(10px)`;
+            element.style.setProperty("--mouse-x", `${mouseX}px`);
+            element.style.setProperty("--mouse-y", `${mouseY}px`);
+        };
+
+
+        const resetTransform = (element: HTMLElement) => {
+            element.style.transform = 'rotateX(0deg) rotateY(0deg) translateZ(0px)';
+        };
+
+        buttons.forEach((button, index) => {
+            const wrapper = wrappers[index];
+            if (!button || !wrapper) return;
+
+            wrapper.addEventListener('mousemove', (e) => handleMouseMove(e, button));
+            wrapper.addEventListener('mouseleave', () => resetTransform(button));
+            button.addEventListener('focus', () => button.style.transform = 'rotateX(0deg) rotateY(0deg) translateZ(10px)');
+            button.addEventListener('blur', () => resetTransform(button));
+        });
+
+        return () => {
+            buttons.forEach((button, index) => {
+                const wrapper = wrappers[index];
+                if (!button || !wrapper) return;
+
+                wrapper.removeEventListener('mousemove', (e) => handleMouseMove(e, button));
+                wrapper.removeEventListener('mouseleave', () => resetTransform(button));
+                button.removeEventListener('focus', () => button.style.transform = 'rotateX(0deg) rotateY(0deg) translateZ(10px)');
+                button.removeEventListener('blur', () => resetTransform(button));
+            });
+        };
+    }, []);
     return (
         <AppValidationObserver methods={methods} onSubmit={onSubmit}>
             {({ register, errors }) => (
@@ -48,22 +107,24 @@ const AppMainForm = ({ btnText }: { btnText:string }) => {
                         type="phone" required={true} />
 
                     <AppTextarea className="!bg-[#2a2a2a] focus:!bg-[#20272a]" title={'Комментарий'} inputName="comment" />
-                    <button type="submit" className="s:mt-[20px] mt-[15px] text-[14px] s:text-[20px] text-[#FFFFFF] font-bold border border-solid border-[#737373] flex items-center gap-[20px] justify-center p-[5px] rounded-[4px]">
-                        {btnText}
-                        <div className="translate-y-[5px]">
-                            <Image alt="message" src={MessageImg} width="0"
-                                        height="0"
-                                        sizes="100vw"
-                                        className="h-[30px] w-[30px] s:h-[40px] s:w-[40px]"/>
-                        </div>
+                    <div ref={setWrapperRef} className="tariff-wrap">
+                        <button type="submit" ref={setButtonRef} className="tariff s:mt-[20px] mt-[15px] text-[14px] s:text-[20px] text-[#FFFFFF] font-bold border border-solid border-[#737373] flex items-center gap-[20px] justify-center p-[5px] rounded-[4px]">
+                            {btnText}
+                            <div className="translate-y-[5px]">
+                                <Image alt="message" src={MessageImg} width="0"
+                                    height="0"
+                                    sizes="100vw"
+                                    className="h-[30px] w-[30px] s:h-[40px] s:w-[40px]" />
+                            </div>
 
 
 
-                    </button>
+                        </button>
+                    </div>
 
 
                     <span className=" mt-[10px] text-[#A4A4A4] text-[10px] s:text-[13px]">
-                    Согласен на обработку моих персональных данных в соответствии с <Link href="/soglashenie/polzovatelskoe-soglashenie/" target="_blank">Пользовательским соглашением</Link>
+                        Согласен на обработку моих персональных данных в соответствии с <Link href="/soglashenie/polzovatelskoe-soglashenie/" target="_blank">Пользовательским соглашением</Link>
                     </span>
 
                 </div>
--- src/components/general/AppHeader.tsx
+++ src/components/general/AppHeader.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import '@/assets/styles/sections/_header.scss'
 import Link from "next/link";
@@ -62,6 +62,64 @@ const AppHeader = () => {
   useEffect(() => {
     closeNavMenues();
   }, [pathname]);
+
+  const buttonRefs = useRef<HTMLAnchorElement[]>([]);
+  const wrapperRefs = useRef<HTMLDivElement[]>([]);
+
+  const setWrapperRef = (el: HTMLDivElement | null) => {
+    if (!el) return
+    wrapperRefs.current.push(el)
+  }
+
+  const setButtonRef = (el: HTMLAnchorElement | null) => {
+    if (!el) return
+    buttonRefs.current.push(el)
+  }
+
+  useEffect(() => {
+    const buttons = buttonRefs.current;
+    const wrappers = wrapperRefs.current;
+
+    if (!buttons.length || !wrappers.length) return;
+
+    const handleMouseMove = (e: MouseEvent, element: HTMLElement) => {
+      const rect = element.getBoundingClientRect();
+      const mouseX = e.clientX - rect.left;
+      const mouseY = e.clientY - rect.top;
+      const rotateX = (mouseY / rect.height) * 30 - 15;
+      const rotateY = (mouseX / rect.width) * -30 + 15;
+      element.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(10px)`;
+      element.style.setProperty("--mouse-x", `${mouseX}px`);
+      element.style.setProperty("--mouse-y", `${mouseY}px`);
+    };
+
+
+    const resetTransform = (element: HTMLElement) => {
+      element.style.transform = 'rotateX(0deg) rotateY(0deg) translateZ(0px)';
+    };
+
+    buttons.forEach((button, index) => {
+      const wrapper = wrappers[index];
+      if (!button || !wrapper) return;
+
+      wrapper.addEventListener('mousemove', (e) => handleMouseMove(e, button));
+      wrapper.addEventListener('mouseleave', () => resetTransform(button));
+      button.addEventListener('focus', () => button.style.transform = 'rotateX(0deg) rotateY(0deg) translateZ(10px)');
+      button.addEventListener('blur', () => resetTransform(button));
+    });
+
+    return () => {
+      buttons.forEach((button, index) => {
+        const wrapper = wrappers[index];
+        if (!button || !wrapper) return;
+
+        wrapper.removeEventListener('mousemove', (e) => handleMouseMove(e, button));
+        wrapper.removeEventListener('mouseleave', () => resetTransform(button));
+        button.removeEventListener('focus', () => button.style.transform = 'rotateX(0deg) rotateY(0deg) translateZ(10px)');
+        button.removeEventListener('blur', () => resetTransform(button));
+      });
+    };
+  }, []);
 
   return (
     <div className={`${burgerMenuActive && 'transparent-header'}`}>
@@ -188,12 +246,18 @@ const AppHeader = () => {
               <span></span>
             </span>
           </button>
-          <Link
-            href="/find-out-cost/"
-            className="py-[13px] px-[24px] bg-[#000000] rounded-[4px] border-solid border border-[#727272] ml-[70px] text-[#FFF] text-[20px] font-bold"
-          >
-            {t('calculation.name')}
-          </Link>
+
+
+          <div ref={setWrapperRef} className="tariff-wrap">
+
+            <Link
+              ref={setButtonRef}
+              href="/find-out-cost/"
+              className="tariff py-[13px] px-[24px] bg-[#000000] rounded-[4px] border-solid border border-[#727272] ml-[70px] text-[#FFF] text-[20px] font-bold"
+            >
+              {t('calculation.name')}
+            </Link>
+          </div>
         </div>
       </header>
     </div>
