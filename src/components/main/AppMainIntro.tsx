69d983b98ae6dff713c88c9cf9e91730331b089c
Zember007 <goraegorgora@mail.ru>
2025-03-25 15:55:44 +0400

    commit

--- src/components/main/AppMainIntro.tsx
+++ src/components/main/AppMainIntro.tsx
@@ -29,7 +29,7 @@ const AppMainIntro = () => {
                         <h1 className="main-banner__title">{filterPrepositions(t('mainIntro.title'))}</h1>
                         <div className='border-main bg-[#00000080] py-[30px] px-[48px] max-w-[420px] flex flex-col gap-[20px] rounded-[6px]'>
                             <span className='text-[#FFF] text-[32px] text-center'>Оставить заявку</span>
-                            <AppMainForm btnText={'Отправить заявку'} />                            
+                            <AppMainForm btnText={'Оформить заявку'} />                            
                         </div>
                     </div>
                 </div >
