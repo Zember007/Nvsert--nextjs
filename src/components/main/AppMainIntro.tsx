fe71dd4bdac8056a5ae5987c2313ac0ab3ee3266
Zember007 <goraegorgora@mail.ru>
2025-03-28 16:58:09 +0400

    dots button

--- src/components/main/AppMainIntro.tsx
+++ src/components/main/AppMainIntro.tsx
@@ -24,6 +24,8 @@ const AppMainIntro = () => {
     return (
         <>
             <section className="main-banner">
+            
+
                 <div className="wrapper">
                     <div className='main-banner__content'>
                         <h1 className="main-banner__title">{filterPrepositions(t('mainIntro.title'))}</h1>
