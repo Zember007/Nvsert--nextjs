f7a6cb08f970aa5cf502d27266ec812a168731c1
Zember007 <goraegorgora@mail.ru>
2025-04-24 17:33:33 +0400

    commit

--- src/components/forms/AppMainForm.tsx
+++ src/components/forms/AppMainForm.tsx
@@ -202,9 +202,9 @@ const AppMainForm = ({ btnText }: { btnText: string }) => {
 
                         <div ref={setWrapperRef} className="tariff-wrap relative">
                             <button
-                                onClick={() => {
-                                    successVisible()
-                                }}
+                                // onClick={() => {
+                                //     successVisible()
+                                // }}
                                 type="submit"
                                 ref={setButtonRef}
                                 className=" group tariff s:mt-[1px] mt-[15px] bg-[#34446D] text-[14px] s:text-[20px] text-[#FFFFFF] font-bold border border-solid border-[#737373] flex items-center gap-[10px] justify-center p-[9px] rounded-[4px]"
