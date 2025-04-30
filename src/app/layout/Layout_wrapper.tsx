f53455906ff4eb5efb8cc9bc88c4e976e225026a
Zember007 <goraegorgora@mail.ru>
2025-04-30 13:10:15 +0400

    commit

--- src/app/layout/Layout_wrapper.tsx
+++ src/app/layout/Layout_wrapper.tsx
@@ -1,4 +1,6 @@
-import '@/assets/styles/base.scss'
+import '@/assets/styles/base.scss';
+import 'simplebar-react/dist/simplebar.min.css';
+import SimpleBar from 'simplebar-react';
 import AppHeader from '@/components/general/AppHeader';
 import { ReactNode, useEffect, useMemo, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -11,61 +13,82 @@ import AppModalWrapper from '@/components/general/AppModalWrapper';
 import { AppDispatch, RootState } from '@/config/store';
 import { usePathname } from 'next/navigation';
 
-
 const Layout_wrapper = ({ children }: { children: ReactNode }) => {
-
-
-    const dispatch = useDispatch<AppDispatch>()
-    const pathname = usePathname()
+    const dispatch = useDispatch<AppDispatch>();
+    const pathname = usePathname();
+    const simpleBarRef = useRef<any>(null); // Ref для SimpleBar
 
     const metadata = useSelector((state: RootState) => state.metadata);
-
-
     const { transparent, setDefaultModalActive, defaultModalActive, defaultModalName } = useHeaderContext();
     const { calcPageBodyClass } = useSelector((state: RootState) => state.documents);
-
-    const { configs: configsPure, file_configs: fileConfigsPure, status, error } = useSelector((state: RootState) => state.config);
+    const { configs: configsPure, file_configs: fileConfigsPure } = useSelector((state: RootState) => state.config);
 
     const configs = useMemo(() => {
         let parsedConf: any = {};
         configsPure?.forEach((item) => {
-            let key = item.key;
-            let value = item.value;
-            parsedConf[key] = value;
+            parsedConf[item.key] = item.value;
         });
-
         return parsedConf;
-    }, [configsPure])
+    }, [configsPure]);
 
     const file_configs = useMemo(() => {
         let parsedConf: any = {};
         fileConfigsPure?.forEach((item) => {
-            let key = item.key;
-            let value = item.value;
-            parsedConf[key] = value;
+            parsedConf[item.key] = item.value;
         });
-
         return parsedConf;
-    }, [fileConfigsPure])
+    }, [fileConfigsPure]);
 
     useEffect(() => {
         if (configs.length && file_configs.length) {
-            dispatch(setMetadata(generateMetadata(configs, file_configs)))
+            dispatch(setMetadata(generateMetadata(configs, file_configs)));
         }
-    }, [configs, file_configs, dispatch])
+    }, [configs, file_configs, dispatch]);
 
     useEffect(() => {
+        if (typeof window === 'undefined') return;
 
-        if (typeof window === "undefined") return
 
+
+        // Обновление конфигураций
+        dispatch(updateActionConfigs());
+        dispatch(updateActionFileConfigs());
+
+        // Установка кастомной высоты для --vh
+        function set100Vh() {
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        }
+
+        set100Vh();
+        window.addEventListener('resize', set100Vh);
+
+        // Отключение контекстного меню для изображений
+        const img = document.querySelector('img');
+        img?.addEventListener('contextmenu', (e) => e.preventDefault());
+
+        // Очистка обработчиков событий при размонтировании
+        return () => {
+            window.removeEventListener('resize', set100Vh);
+            img?.removeEventListener('contextmenu', (e) => e.preventDefault());
+        };
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (!simpleBarRef.current) return
         let currentScroll = 0;
         let targetScroll = 0;
         let isScrolling = false;
 
+        // Получаем контейнер прокрутки SimpleBar
+        const scrollContainer = simpleBarRef.current?.getScrollElement();
+
         // Инициализируем текущую прокрутку
         const initScroll = () => {
-            currentScroll = window.scrollY;
-            targetScroll = currentScroll;
+            if (scrollContainer) {
+                currentScroll = scrollContainer.scrollTop;
+                targetScroll = currentScroll;
+            }
         };
 
         const smoothScroll = () => {
@@ -74,8 +97,10 @@ const Layout_wrapper = ({ children }: { children: ReactNode }) => {
                 isScrolling = false;
                 return;
             }
-            currentScroll += diff * 0.1;
-            window.scrollTo(0, currentScroll);
+            currentScroll += diff * 0.1; // Плавное движение (0.1 — коэффициент сглаживания)
+            if (scrollContainer) {
+                scrollContainer.scrollTo({ top: currentScroll });
+            }
             requestAnimationFrame(smoothScroll);
         };
 
@@ -83,9 +108,11 @@ const Layout_wrapper = ({ children }: { children: ReactNode }) => {
             e.preventDefault();
             targetScroll += e.deltaY;
 
-            // Ограничим targetScroll в пределах документа
-            const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
-            targetScroll = Math.max(0, Math.min(targetScroll, maxScroll));
+            // Ограничим targetScroll в пределах контейнера
+            if (scrollContainer) {
+                const maxScroll = scrollContainer.scrollHeight - scrollContainer.clientHeight;
+                targetScroll = Math.max(0, Math.min(targetScroll, maxScroll));
+            }
 
             if (!isScrolling) {
                 isScrolling = true;
@@ -93,59 +120,45 @@ const Layout_wrapper = ({ children }: { children: ReactNode }) => {
             }
         };
 
-        initScroll();
-        window.addEventListener('wheel', handleWheel, { passive: false });
-        window.addEventListener('scroll', () => {
-            if (!isScrolling) {
-                currentScroll = window.scrollY;
+        const handleScroll = () => {
+            if (!isScrolling && scrollContainer) {
+                currentScroll = scrollContainer.scrollTop;
                 targetScroll = currentScroll;
             }
-        });
+        };
 
+        initScroll();
 
-        dispatch(updateActionConfigs())
-        dispatch(updateActionFileConfigs())
-
-        function set100Vh() {
-            let vh = window.innerHeight * 0.01;
-            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        // Добавляем обработчики событий к контейнеру SimpleBar
+        if (scrollContainer) {
+            scrollContainer.addEventListener('wheel', handleWheel, { passive: false });
+            scrollContainer.addEventListener('scroll', handleScroll);
         }
 
+        return () => {
+            if (scrollContainer) {
+                scrollContainer.removeEventListener('wheel', handleWheel);
+                scrollContainer.removeEventListener('scroll', handleScroll);
+            }
+        };
+    }, [simpleBarRef])
 
-
-        set100Vh();
-
-        window.addEventListener('resize', set100Vh);
-
-        const img = document.querySelector('img');
-
-        img?.addEventListener('contextmenu', function (e) {
-            e.preventDefault();
-        });
-
-
-    }, [])
-
-
-
-    const [classBody, setClassBody] = useState('transparent-header')
+    const [classBody, setClassBody] = useState('transparent-header');
 
     useEffect(() => {
         if (pathname !== '/' && classBody !== '') {
-
-            setClassBody('')
-
+            setClassBody('');
         }
-
         if (pathname === '/' && classBody === '') {
-            setClassBody('transparent-header')
+            setClassBody('transparent-header');
         }
-    }, [pathname])
+    }, [pathname]);
+
     return (
         <>
             <head>
                 <title>{metadata.title}</title>
-                <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;1,300&display=swap" rel="stylesheet"></link>
+                <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;1,300&display=swap" rel="stylesheet" />
                 <meta name="description" content={metadata.description} />
                 <meta name="keywords" content={metadata.keywords} />
                 <meta property="og:title" content={metadata.openGraph?.title} />
@@ -171,13 +184,15 @@ const Layout_wrapper = ({ children }: { children: ReactNode }) => {
                 <link rel="apple-touch-icon" sizes="1024x1024" href="/apple-touch-icon-1024x1024.png" />
             </head>
             <body className={classBody}>
-                <main className={`${transparent && 'transparent-header'}  ${calcPageBodyClass && 'cost-calc-page'}`}>
-                    <div className="content">
-                        <AppHeader />
-                        {children}
-                    </div>
-                    <AppFooter />
-                </main>
+                <SimpleBar className="max-h-[100vh]" ref={simpleBarRef}>
+                    <main className={`${transparent && 'transparent-header'} ${calcPageBodyClass && 'cost-calc-page'}`}>
+                        <div className="content">
+                            <AppHeader />
+                            {children}
+                        </div>
+                        <AppFooter />
+                    </main>
+                </SimpleBar>
                 <AppModalWrapper
                     setDefaultModalActive={setDefaultModalActive}
                     defaultModalActive={defaultModalActive}
@@ -189,4 +204,3 @@ const Layout_wrapper = ({ children }: { children: ReactNode }) => {
 };
 
 export default Layout_wrapper;
-
