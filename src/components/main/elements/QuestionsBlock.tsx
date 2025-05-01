1c2d1f4d1b8ac82a33f396efbf0f09e069a81fdb
Zember007 <goraegorgora@mail.ru>
2025-05-01 13:21:25 +0400

    commit

--- src/components/main/elements/QuestionsBlock.tsx
+++ src/components/main/elements/QuestionsBlock.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const QuestionsBlock = ({ setActive, active }: { setActive: (value: boolean) => void, active: boolean }) => {
+const QuestionsBlock = ({ setActive, active, number, title, node }: { setActive: (value: boolean) => void, active: boolean, number:number, title:string, node:string }) => {
     return (
         <div className={`relative flex flex-col rounded-[4px] bg-[#FFFFFF] z-[0]`}>
             <div className={`absolute transition-all duration-200 top-0 left-0 right-0 bottom-0 rounded-[4px] z-[-1] border border-solid border-[transparent] ${active && '!border-[#34446D]'}`}></div>
@@ -10,10 +10,10 @@ const QuestionsBlock = ({ setActive, active }: { setActive: (value: boolean) => 
                 }}
                 className={`${active && ' !bg-[#34446D] !border-[#000]'} bg-[#FFFFFF26] hover:bg-[#34446D33] h-[70px] relative active:transition-all duration-300 active:bg-[#5B6788] active:shadow-[inset_-2px_-2px_4px_#000,inset_2px_2px_4px_#000] hover:border-[#34446D] backdrop-blur-[1px] group border-[#CCCCCC] cursor-pointer border-solid border rounded-[4px]  flex items-center gap-[40px] pr-[10px]`}>
                 <div className="w-[70px] h-[70px] flex items-center justify-center">
-                    <p className={`${active && 'text-[50px] text-[#FFF]'} text-[22px] rubik group-hover:text-[50px] group-active:duration-[0] ease-in-out transition-all duration-300 group-active:text-[#FFF]`}>1</p>
+                    <p className={`${active && 'text-[50px] text-[#FFF]'} text-[22px] rubik group-hover:text-[50px] group-active:duration-[0] ease-in-out transition-all duration-300 group-active:text-[#FFF]`}>{number}</p>
                 </div>
                 <div className="group-active:scale-[0.99] grow will-change-transform ease transition-all duration-100  flex items-center gap-[40px]">
-                    <p className={`${active ? '[text-shadow:0_0_0.7px_white] text-[#FFF]' : 'group-hover:[text-shadow:0_0_0.7px_black]'} grow text-[18px] ease-in-out transition-all duration-300 group-active:duration-[0] font-[400] group-active:text-[#FFF]  group-active:[text-shadow:0_0_0.7px_white]`}>Как проходит сертификация продукции?</p>
+                    <p className={`${active ? '[text-shadow:0_0_0.7px_white] text-[#FFF]' : 'group-hover:[text-shadow:0_0_0.7px_black]'} grow text-[18px] ease-in-out transition-all duration-300 group-active:duration-[0] font-[400] group-active:text-[#FFF]  group-active:[text-shadow:0_0_0.7px_white]`}>{title}</p>
                     <button>
                         <svg
                             className={`${!active ? 'rotate-[180deg] group-hover:*:stroke-[#000] ' : '*:stroke-[#FFF]'} *:transition-all *:duration-200  group-active:*:stroke-[#FFF] transition-all duration-300`}
@@ -25,8 +25,10 @@ const QuestionsBlock = ({ setActive, active }: { setActive: (value: boolean) => 
                     </button>
                 </div>
             </div>
-            <div className={`max-h-[0px] transition-all duration-200 delay-100 ${active && '!max-h-[1000px]'}`}>
-                <div className="px-[110px] py-[30px]"></div>
+            <div className={`max-h-[0px] transition-all duration-200 delay-100 overflow-hidden ${active && '!max-h-[1000px]'}`}>
+                <div className="px-[110px] py-[30px]" dangerouslySetInnerHTML={{ __html: node }}>
+    
+                </div>
             </div>
         </div>
     );
