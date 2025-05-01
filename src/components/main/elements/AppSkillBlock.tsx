ec96b67af406a1c466526a805f18fcd9d00677df
Zember007 <goraegorgora@mail.ru>
2025-05-01 14:50:37 +0400

    commit

--- src/components/main/elements/AppSkillBlock.tsx
+++ src/components/main/elements/AppSkillBlock.tsx
@@ -92,7 +92,7 @@ const AppSkillBlock = (skill: any) => {
             }),
             ...(mousePX && { ...cardStyle })
           }}
-          className={`flex   ${!skill.folder && 'border border-solid'} backdrop-blur-[2px] rounded-[4px] overflow-hidden flex-col gap-[14px] relative z-[1] justify-between h-full transition-all duration-500 ${skill.folder ? '!items-center !justify-center' : 'p-[20px]'}`}
+          className={`flex   ${!skill.folder && 'border border-solid'} backdrop-blur-[1px] rounded-[4px] overflow-hidden flex-col gap-[14px] relative z-[1] justify-between h-full transition-all duration-500 ${skill.folder ? '!items-center !justify-center' : 'p-[20px]'}`}
         >
           {
             skill.folder ?
