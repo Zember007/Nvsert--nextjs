0699df4f5375da347884ea6401131a5f88df9773
Zember007 <goraegorgora@mail.ru>
2025-05-23 15:38:45 +0400

    commit

--- src/hook/filter.ts
+++ src/hook/filter.ts
@@ -6,6 +6,7 @@ export const filterEmail = (value = '') => {
     return 'mailto:' + value;
 }
 
+
 export const filterPrepositions = (text: string): string => {
     return text.replace(/(^|\s)(и|в|с|к|у|о|а|но|на|по|из|от|за|до|не|да|же|ли|бы)\s/g, "$1$2\u00A0");
 };
