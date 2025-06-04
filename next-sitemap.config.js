c1dc6d780d8bfeaa6f3b5ede3bb01e16cbad7680
Zember007 <goraegorgora@mail.ru>
2025-06-04 15:56:02 +0400

    commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next-sitemap').IConfig} */
+const nextConfig = {
+    siteUrl: process.env.baseUrl || 'https://example.com',
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+        additionalSitemaps: [
+            `${process.env.baseUrl}/sitemap.xml`,
+        ],
+        policies: [
+            {
+                userAgent: '*',
+                disallow: ['/adm/', '/*?*'],
+                allow: ['*/?page'],
+            },
+            {
+                userAgent: 'Yandex',
+                disallow: ['/adm/', '/*?*'],
+                allow: ['*/?page'],
+            },
+            {
+                userAgent: 'Googlebot',
+                disallow: ['/adm/', '/*?*'],
+                allow: ['*/?page', '*/?utm'],
+            },
+        ],
+    },
+}
+export default nextConfig
