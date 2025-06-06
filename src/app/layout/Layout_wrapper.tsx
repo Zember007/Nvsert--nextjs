7c3024490beae13d4388eb4948b71bb115aa40bc
Zember007 <goraegorgora@mail.ru>
2025-06-06 15:55:05 +0400

    commit

--- src/app/layout/Layout_wrapper.tsx
+++ src/app/layout/Layout_wrapper.tsx
@@ -190,22 +190,7 @@ const Layout_wrapper = ({ children }: { children: ReactNode }) => {
                 {metadata.openGraph?.images?.map((image, i) => (
                     <meta key={i} property="og:image" content={image} />
                 ))}
-                <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-                <link rel="icon" sizes="16x16" type="image/png" href="/favicon-16x16.png" />
-                <link rel="icon" sizes="32x32" type="image/png" href="/favicon-32x32.png" />
-                <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png" />
-                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-                <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
-                <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png" />
-                <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
-                <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
-                <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
-                <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
-                <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
-                <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
-                <link rel="apple-touch-icon" sizes="167x167" href="/apple-touch-icon-167x167.png" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
-                <link rel="apple-touch-icon" sizes="1024x1024" href="/apple-touch-icon-1024x1024.png" />
+               <link rel="icon" href="/favicon.ico" />
             </head>
             <body className={classBody}>
                 <SimpleBarContext.Provider value={{ simpleBar: simpleBarRef.current }}>
