36ca9cc851f612481c4294fd97ac0accfba417eb
Zember007 <goraegorgora@mail.ru>
2025-06-06 16:49:20 +0400

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,16 @@ const nextConfig = {
   },
 
   webpack: (config) => {
-    return config;
-  },
+    config.module.rules.push({
+      test: /\.mp4$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/media/[name].[hash][ext]',
+      },
+    });
 
-  
+    return config;
+  }
 
 };
 
