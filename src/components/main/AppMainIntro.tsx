8e28632fec6eb29528f652f1465edf8bc6ff044c
Zember007 <goraegorgora@mail.ru>
2025-06-20 17:09:44 +0400

    commit

--- src/components/main/AppMainIntro.tsx
+++ src/components/main/AppMainIntro.tsx
@@ -95,7 +95,7 @@ const AppMainIntro = () => {
 
                 <div className="wrapper">
                     <div className='main-banner__content'>
-                        <h1 className="main-banner__title">{t('mainIntro.title')}</h1>
+                        <h1 className="main-banner__title">{filterPrepositions(t('mainIntro.title'))}</h1>
 
                         <motion.div
                             animate={controls}>
