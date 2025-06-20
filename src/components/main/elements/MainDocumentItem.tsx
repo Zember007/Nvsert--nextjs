f71c0155018f1bbdf35c6f5bb29fe9aee3f2624b
Zember007 <goraegorgora@mail.ru>
2025-06-20 18:55:57 +0400

    commit

--- src/components/main/elements/MainDocumentItem.tsx
+++ src/components/main/elements/MainDocumentItem.tsx
@@ -184,7 +184,7 @@ const MainDocumentItem = memo(({
         >
 
             <div
-                className={`document__border ${!active ? 'group-hover/main:!border-[transparent]' : 'active'}`}
+                className={`document__border ${!active ? 'group-active/main:!border-[transparent] m:group-hover/main:!border-[transparent]' : 'active'}`}
             />
 
             <div
