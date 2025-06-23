2805ce90b61068ab670c5c061367f48abe8b6419
Zember007 <goraegorgora@mail.ru>
2025-06-23 12:59:40 +0400

    commit

--- src/components/main/elements/MainDocumentItem.tsx
+++ src/components/main/elements/MainDocumentItem.tsx
@@ -77,7 +77,7 @@ const DocumentList = memo(({ content1, listHidden, setListHidden }: DocumentList
                         onClick={() => setListHidden(!listHidden)}
                     >
                         {listHidden ? 'Показать полный список документов' : 'Скрыть'}
-                        <svg className={`${!listHidden ? 'rounded-[180deg]' : ''}`} xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#34446D" version="1.1" id="Layer_1" width="16" height="16" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
+                        <svg className={`${!listHidden ? 'rounded-[180deg]' : ''}`} xmlns="http://www.w3.org/2000/svg" fill="#34446D" version="1.1" id="Layer_1" width="16" height="16" viewBox="0 0 100 100" enable-background="new 0 0 100 100" >
                             <g>
                                 <path d="M50.5,19.881c-1.104,0-2,0.896-2,2V72.17L33.193,56.609c-0.781-0.781-1.922-0.781-2.703,0   c-0.781,0.78-0.719,2.047,0.062,2.828l18.883,18.857c0.375,0.375,0.899,0.586,1.43,0.586s1.047-0.211,1.422-0.586l18.857-18.857   c0.781-0.781,0.783-2.048,0.002-2.828c-0.781-0.781-2.296-0.781-3.077,0L52.5,71.933V21.881C52.5,20.776,51.604,19.881,50.5,19.881   z" />
                             </g>
