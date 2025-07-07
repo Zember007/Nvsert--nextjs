c65347652c7d5c038cb8a01a0be073dde14244e6
Zember007 <goraegorgora@mail.ru>
2025-07-07 15:58:31 +0400

    fix

--- src/components/main/AppMainSlider.tsx
+++ src/components/main/AppMainSlider.tsx
@@ -338,8 +338,8 @@ const SliderMain = () => {
                             onClick={() => { openDefaultModal('introForm') }}
                             ref={setButtonRef} className=' slider__button group btnIconAn doc-btn tariff'>
                             <span className="sendIconLeft">
-                                <svg className='group-hover:*:fill-[#FFF] rotate-[45deg] *:transition-all *:duration-300' width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M29.0627 0.9375L0.930664 12.1875L11.426 16.9336L26.2502 3.75L13.0666 18.5742L17.8127 29.0625L29.0627 0.9375Z" fill="#34446D" />
+                                <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M3 9.48438V7.48438H0V9.48438H3ZM8.96767 1.48438L7.52908 2.91514L12.1092 7.47151H6V9.49623H12.1092L7.52908 14.0526L8.96767 15.4844L16 8.48438L15.2822 7.76899L14.5634 7.0526L8.96767 1.48438Z" fill="white" />
                                 </svg>
                             </span>
                             <span
