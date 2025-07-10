190a7d654bd798bfdee6b0e070ae9afdaaa18fd8
Zember007 <goraegorgora@mail.ru>
2025-07-10 14:55:00 +0400

    fix

--- src/components/main/elements/QuestionsBlock.tsx
+++ src/components/main/elements/QuestionsBlock.tsx
@@ -53,7 +53,7 @@ const QuestionsBlock = ({ setActive, active, number, title, text }: { setActive:
                         <p className={`${active && 'm:text-[48px] text-[#FFF]'} font-light text-[24px] rubik m:group-hover:text-[50px] group-active:duration-[0] ease transition-all duration-100 group-active:text-[#FFF]`}>{number}</p>
                     </div>
                     <div className={`${active ? 'scale-[0.99]' : ''} transition-scale group-active:scale-[0.99] grow will-change-transform ease transition-all duration-100  flex items-center gap-[40px]`}>
-                        <h3 className={`${active ? '[text-shadow:0_0_0.7px_white] text-[#FFF]' : ''} grow text-[18px] m:text-[20px] ease-in-out transition-all duration-200 group-active:duration-[0] font-[400] group-active:text-[#FFF]  group-active:[text-shadow:0_0_0.7px_white]`}>{title}</h3>
+                        <h3 className={`${active ? 'text-[#FFF]' : ''} font-light grow text-[18px] m:text-[20px] ease-in-out transition-all duration-200 group-active:duration-[0] font-[400] group-active:text-[#FFF]  `}>{title}</h3>
 
                         <svg
                             className={`${!active ? '' : '*:stroke-[#FFF] rotate-[180deg]'}  hidden m:block transition-all duration-200`}
