bfeca70f50ff51f5396b6a1d031640b777f89e8e
Georgii Borisov <goraegorgora@mail.ru>
2025-07-28 10:19:19 +0400

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,21 +5,21 @@ import '@/assets/styles/base/_mixins.scss'
 import '@/assets/styles/base/_icon.scss'
 import { ReactNode } from "react"
 
-import { Rubik } from 'next/font/google'
+/* import { Rubik } from 'next/font/google'
 
 const rubik = Rubik({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
   display: 'swap',
   variable: '--font-rubik',
-})
+}) */
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 
 
 
   return (
-    <html lang="ru" className={rubik.variable}>
+    <html lang="ru" >
       <Provider>
 
 
