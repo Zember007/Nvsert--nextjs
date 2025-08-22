7b5f264e5128bc85677fc5fe825a307ce798e672
Georgii Borisov <goraegorgora@mail.ru>
2025-08-22 15:36:22 +0400

    fix

--- src/components/services/ServiceItem.tsx
+++ src/components/services/ServiceItem.tsx
@@ -79,7 +79,7 @@ const ServiceItem: React.FC<ServiceItemProps> = ({ service, index, isExpanded, o
                         {service.items.map((item, itemIndex) => (
                             <AppNavigationItem
                                 dark={true}
-                                className='!w-[252px]'
+                                className='!w-[250px]'
                                 key={itemIndex}
                                 title={item.title}
                                 img={'https://test11.audiosector.ru/cp' + item.img?.url}
