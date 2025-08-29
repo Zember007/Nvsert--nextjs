b368c8d0fd364188249847226a40a9b7c6c03b15
Georgii Borisov <goraegorgora@mail.ru>
2025-08-29 14:40:53 +0400

    fix

--- src/components/general/AppNavigation.tsx
+++ src/components/general/AppNavigation.tsx
@@ -78,7 +78,7 @@ const AppNavigationItem = ({ img, title, controls, link, dark, className }: { li
                         animate={controls}>
                         <Image src={img as string} className="w-[43px] h-[60px]" width={43} height={60} alt="document" />
                     </motion.div>
-                    <p className={`${dark ? 'text-[#000]' : 'text-[#FFF]'} whitespace-pre-line max-w-full  xl:text-[18px] text-[14px]`}>{filterPrepositions(title)}</p>
+                    <p className={`${dark ? 'text-[#000]' : 'text-[#FFF]'} whitespace-pre-line max-w-full xl:pr-[20px]  xl:text-[18px] text-[14px]`}>{filterPrepositions(title)}</p>
                 </Link>
             </div>
         </>
