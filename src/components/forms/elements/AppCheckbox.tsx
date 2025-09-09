de9ad4d4b1e7eee305dcd8081c319a6a14acc5ba
Georgii Borisov <goraegorgora@mail.ru>
2025-09-09 13:41:35 +0400

    fix

--- src/components/forms/elements/AppCheckbox.tsx
+++ src/components/forms/elements/AppCheckbox.tsx
@@ -30,7 +30,7 @@ const CustomCheckbox: React.FC<CustomCheckboxProps> = ({
     const labelRef = React.useRef<HTMLLabelElement>(null);
 
     return (
-        <div className="flex items-center s:gap-[6px] gap-[4px] no-drag"
+        <div className="flex s:gap-[6px] gap-[4px] no-drag"
             onClick={(e) => {
                 if (labelRef.current?.contains(e.target as Node)) return;
                 onChange(!checked)
