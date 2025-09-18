ad38fe034e31d402c462b8007674104c0c311803
Georgii Borisov <goraegorgora@mail.ru>
2025-09-18 16:12:56 +0400

    fix

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,25 +3,31 @@ import { useButton } from '@/hook/useButton';
 
 type TariffButtonProps = {
     label?: string;
+    icon?: React.ReactNode;
     wrapperClassName?: string;
     buttonClassName?: string;
 };
 
 const Button: FC<TariffButtonProps> = ({
     label = 'Подробнее',
-    wrapperClassName = 'skills-tariff-wrap tariff-wrap ',
-    buttonClassName = 'slider__button group btnIconAn doc-btn tariff'
+    icon,
+    wrapperClassName,
+    buttonClassName
 }) => {
     const { setButtonRef, setWrapperRef } = useButton();
 
     return (
-        <div className={wrapperClassName} ref={setWrapperRef}>
-            <button ref={setButtonRef} className={buttonClassName}>
+        <div className={`skills-tariff-wrap main-button-wrap tariff-wrap ${wrapperClassName}`} ref={setWrapperRef}>
+            <button ref={setButtonRef} className={` main-button btnIconAn doc-btn tariff ${buttonClassName}`}>
                 <span className="sendText">{label}</span>
                 <span className="sendIconLeft">
-                    <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M3 9.48438V7.48438H0V9.48438H3ZM8.96767 1.48438L7.52908 2.91514L12.1092 7.47151H6V9.49623H12.1092L7.52908 14.0526L8.96767 15.4844L16 8.48438L15.2822 7.76899L14.5634 7.0526L8.96767 1.48438Z" fill="white" />
-                    </svg>
+                    {icon ?
+                        icon :
+
+                        <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M3 9.48438V7.48438H0V9.48438H3ZM8.96767 1.48438L7.52908 2.91514L12.1092 7.47151H6V9.49623H12.1092L7.52908 14.0526L8.96767 15.4844L16 8.48438L15.2822 7.76899L14.5634 7.0526L8.96767 1.48438Z" fill="white" />
+                        </svg>
+                        }
                 </span>
             </button>
         </div>
