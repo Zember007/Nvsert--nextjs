480d45c4617ea7b54b276d11c65b1c8111516b79
Georgii Borisov <goraegorgora@mail.ru>
2025-10-20 15:05:09 +0400

    Fixing

--- src/app/services/[slug]/ClientPage.tsx
+++ src/app/services/[slug]/ClientPage.tsx
@@ -272,7 +272,7 @@ const ServiceDetailContent: React.FC<ClientPageProps> = ({ initialNavigation, in
                                                 {index === ctaInsertAfterIndex && (
                                                     <div className="text-center   max-w-[700px] w-full min-h-[300px] bg-[rgba(52,68,109,0.2)] rounded-[8px] flex flex-col justify-center items-center gap-[16px] s:p-[40px] p-[20px] backdrop-blur-sm" key="cta-banner">
                                                         <h3 className="s:text-[24px] text-[20px] font-light tracking-[-0.04em]  text-black max-w-[460px]">
-                                                            {currentService?.cta?.title}
+                                                            {currentService?.cta?.text}
                                                         </h3>
                                                         <p className="s:text-[16px] text-[14px] font-light tracking-[-0.01em]  text-[rgba(0,0,0,0.6)] max-w-[378px] leading-[1.4]">
                                                             {currentService?.cta?.description}
