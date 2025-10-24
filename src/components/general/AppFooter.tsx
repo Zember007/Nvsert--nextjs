1b560f1be793f400934a2efa96285f5b417f0f57
Georgii Borisov <goraegorgora@mail.ru>
2025-10-24 16:35:58 +0400

    fix

--- src/components/general/AppFooter.tsx
+++ src/components/general/AppFooter.tsx
@@ -210,7 +210,7 @@ const AppFooter = () => {
                 </>
             }} isActive={false} />
           <PromtModal
-            classNameBox="!w-auto xl:!relative !absolute xl:bottom-0 xl:right-0 m:bottom-[-53px] m:bottom-[-66px] m:right-[34px] bottom-[-80px]  right-1/2 m:translate-x-0 translate-x-[140px]"
+            classNameBox="!w-auto xl:!relative !absolute xl:bottom-0 xl:right-0 m:bottom-[-53px]  m:right-[34px] bottom-[-80px]  right-1/2 m:translate-x-0 translate-x-[140px]"
             className='!py-[20px]'
             content={
               <div className="flex flex-col gap-[20px]">
