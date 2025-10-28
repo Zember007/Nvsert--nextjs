863d471394041b6442f2b9e78318b158b47b6a1b
Georgii Borisov <goraegorgora@mail.ru>
2025-10-28 14:03:12 +0400

    add partners

--- src/components/services/ServiceCard.tsx
+++ src/components/services/ServiceCard.tsx
@@ -31,7 +31,7 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ serviceName, certificate, cla
         href ? (
             <Link
                 href={href}
-                className={` transition-transform will-change-transform duration-100 active:scale-[.95] ${padding ? 'xss:p-[30px] p-[15px] xss:w-[312px] w-[282px] hover:bg-[#F5F5F2] border border-[transparent] hover:border-[#34446D]' : 'p-0 w-full'} flex flex-col gap-[20px] text-left  rounded-[8px]  ${className || ''}`}
+                className={` transition-transform will-change-transform duration-100 active:scale-[.95] ${padding ? 'xss:px-[30px] xss:py-[30px] px-[15px] py-[20px] xss:w-[312px] w-[282px] hover:bg-[#F5F5F2] border border-[transparent] hover:border-[#34446D]' : 'p-0 w-full'} flex flex-col gap-[20px] text-left  rounded-[8px]  ${className || ''}`}
             >
                 {title && <h6 className='flex items-center h-[38px] max-w-[230px] whitespace-pre-line !font-normal'>{replaceValue(certificate.title)}</h6>}
                 <div className="relative w-full ">
@@ -44,7 +44,7 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ serviceName, certificate, cla
                             className='h-[346px]'
                         />
                     </div>
-                    <div className='w-[250px] justify-between flex absolute bottom-[-10px] *:*:!leading-[0.68]  right-[-11px] flex p-[11px] bg-[#F5F5F580] rounded-[4px] border border-[#000] backdrop-blur-[4px]'>
+                    <div className='xss:w-[250px] w-[206px] justify-between flex absolute xss:bottom-[-10px] bottom-[-13px] xss:*:*:!leading-[0.68] *:*:!leading-[0.8]  xss:right-[-11px] right-[-8px] flex p-[11px] bg-[#F5F5F580] rounded-[4px] border border-[#000] backdrop-blur-[4px]'>
                         <div className="flex flex-col gap-[10px]">
                             <span className='text-5'>Срок оформления</span>
                             <span className='text-1 font-light'>{certificate.duration}</span>
@@ -59,7 +59,7 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ serviceName, certificate, cla
         ) : (
                 <button
                     onClick={onClick}
-                    className={` transition-transform will-change-transform duration-100 active:scale-[.95] ${padding ? 'xss:p-[30px] p-[15px] xss:w-[312px] w-[282px] hover:bg-[#F5F5F2] border border-[transparent] hover:border-[#34446D]' : 'p-0 w-full'} flex flex-col gap-[20px] text-left  rounded-[8px]  ${className || ''}`}
+                    className={` transition-transform will-change-transform duration-100 active:scale-[.95] ${padding ? 'xss:px-[30px] xss:py-[30px] px-[15px] py-[20px] xss:w-[312px] w-[282px] hover:bg-[#F5F5F2] border border-[transparent] hover:border-[#34446D]' : 'p-0 w-full'} flex flex-col gap-[20px] text-left  rounded-[8px]  ${className || ''}`}
                 >
                     {title && <h6 className='flex items-center h-[38px] max-w-[230px] whitespace-pre-line'>{replaceValue(certificate.title)}</h6>}
                     <div className="relative w-full ">
@@ -72,7 +72,7 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ serviceName, certificate, cla
                                 className='h-[346px]'
                             />
                         </div>
-                        <div className='w-[250px] justify-between flex absolute bottom-[-10px] *:*:!leading-[0.68]  right-[-11px] flex p-[11px] bg-[#F5F5F580] rounded-[4px] border border-[#000] backdrop-blur-[4px]'>
+                        <div className='xss:w-[250px] w-[206px] justify-between flex absolute xss:bottom-[-10px] bottom-[-13px] xss:*:*:!leading-[0.68] *:*:!leading-[0.8]  xss:right-[-11px] right-[-8px] flex p-[11px] bg-[#F5F5F580] rounded-[4px] border border-[#000] backdrop-blur-[4px]'>
                             <div className="flex flex-col gap-[10px]">
                                 <span className='text-5'>Срок оформления</span>
                                 <span className='text-1 font-light'>{certificate.duration}</span>
