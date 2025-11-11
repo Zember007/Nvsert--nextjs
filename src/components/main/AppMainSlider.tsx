82575e9f3dc4a38be75188adbff61f7d051a4ccd
Georgii Borisov <goraegorgora@mail.ru>
2025-11-11 15:48:18 +0400

    fix

--- src/components/main/AppMainSlider.tsx
+++ src/components/main/AppMainSlider.tsx
@@ -263,18 +263,18 @@ const SliderMain = () => {
                                         aria-label="previous slide" data-slider="button-prev"
                                         className="tariff item group">
 
-                                        <div className="navigation-button-icon">
+                                        <span className="navigation-button-icon">
                                             <svg
                                                 width="12" height="24" viewBox="0 0 12 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M10 21.957L2 13.404L2.00006 9.60268L10 2" stroke="white" strokeWidth="2.66667"  />
                                             </svg>
-                                        </div>
-                                        <div className="navigation-button-icon">
+                                        </span>
+                                        <span className="navigation-button-icon">
                                             <svg
                                                 width="12" height="24" viewBox="0 0 12 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M10 21.957L2 13.404L2.00006 9.60268L10 2" stroke="white" strokeWidth="2.66667"  />
                                             </svg>
-                                        </div>
+                                        </span>
 
                                     </button>
                                 </div>
@@ -286,18 +286,18 @@ const SliderMain = () => {
                                         aria-label="previous slide" data-slider="button-next"
                                         className=" tariff item group">
 
-                                        <div className="navigation-button-icon-next">
+                                        <span className="navigation-button-icon-next">
                                             <svg
                                                 width="12" height="24" viewBox="0 0 12 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M2 21.957L10 13.404L9.99994 9.60268L2 2" stroke="white" strokeWidth="2.66667"  />
                                             </svg>
-                                        </div>
-                                        <div className="navigation-button-icon-next">
+                                        </span>
+                                        <span className="navigation-button-icon-next">
                                             <svg
                                                 width="12" height="24" viewBox="0 0 12 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M2 21.957L10 13.404L9.99994 9.60268L2 2" stroke="white" strokeWidth="2.66667" />
                                             </svg>
-                                        </div>
+                                        </span>
                                     </button>
                                 </div>
                             </div>
