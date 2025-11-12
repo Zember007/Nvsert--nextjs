857bf453defbfe627b858d18a40b46091c683ea0
Georgii Borisov <goraegorgora@mail.ru>
2025-11-12 13:10:20 +0400

    fix

--- src/components/services/ServiceCard.tsx
+++ src/components/services/ServiceCard.tsx
@@ -44,7 +44,7 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ serviceName, certificate, cla
                             className='h-[346px]'
                         />
                     </div>
-                    <div className='xss:w-[250px] w-[206px] justify-between flex absolute xss:bottom-[-10px] bottom-[-13px] xss:*:*:!leading-[0.68] *:*:!leading-[0.8]  xss:right-[-11px] right-[-8px] flex p-[11px] bg-[#F5F5F580] rounded-[4px] border border-[#000] backdrop-blur-[4px]'>
+                    <div className=' justify-between flex absolute bottom-[9px] left-[9px] right-[9px] xss:*:*:!leading-[0.68] *:*:!leading-[0.8]   flex p-[11px] bg-[#F5F5F580] rounded-[4px] border border-[#000] backdrop-blur-[4px]'>
                         <div className="flex flex-col gap-[10px]">
                             <span className='text-5'>Срок оформления</span>
                             <span className='text-1 font-light'>{certificate.duration}</span>
@@ -57,33 +57,33 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ serviceName, certificate, cla
                 </div>
             </Link>
         ) : (
-                <div
-                    onClick={onClick}
-                    className={` transition-transform will-change-transform duration-100 active:scale-[.95] cursor-pointer ${padding ? 'xss:px-[30px] xss:py-[30px] px-[15px] py-[20px] xss:w-[312px] w-[282px] hover:bg-[#F5F5F2] border border-[transparent] hover:border-[#34446D]' : 'p-0 w-full'} flex flex-col gap-[20px] text-left  rounded-[8px]  ${className || ''}`}
-                >
-                    {title && <h6 className='flex items-center xss:h-[38px] h-[34px] max-w-[230px] whitespace-pre-line'>{replaceValue(certificate.title)}</h6>}
-                    <div className="relative w-full ">
-                        <div className="border border-[#93969d] rounded-[4px] overflow-hidden h-[346px]">
-                            <Image
-                                src={'https://test11.audiosector.ru/cp' + certificate.img?.url}
-                                alt={certificate.title}
-                                width={249}
-                                height={346}
-                                className='h-[346px]'
-                            />
+            <div
+                onClick={onClick}
+                className={` transition-transform will-change-transform duration-100 active:scale-[.95] cursor-pointer ${padding ? 'xss:px-[30px] xss:py-[30px] px-[15px] py-[20px] xss:w-[312px] w-[282px] hover:bg-[#F5F5F2] border border-[transparent] hover:border-[#34446D]' : 'p-0 w-full'} flex flex-col gap-[20px] text-left  rounded-[8px]  ${className || ''}`}
+            >
+                {title && <h6 className='flex items-center xss:h-[38px] h-[34px] max-w-[230px] whitespace-pre-line'>{replaceValue(certificate.title)}</h6>}
+                <div className="relative w-full ">
+                    <div className="border border-[#93969d] rounded-[4px] overflow-hidden h-[346px]">
+                        <Image
+                            src={'https://test11.audiosector.ru/cp' + certificate.img?.url}
+                            alt={certificate.title}
+                            width={249}
+                            height={346}
+                            className='h-[346px]'
+                        />
+                    </div>
+                    <div className=' justify-between flex absolute bottom-[9px] left-[9px] right-[9px] xss:*:*:!leading-[0.68] *:*:!leading-[0.8]   flex p-[11px] bg-[#F5F5F580] rounded-[4px] border border-[#000] backdrop-blur-[4px]'>
+                        <div className="flex flex-col gap-[10px]">
+                            <span className='text-5'>Срок оформления</span>
+                            <span className='text-1 font-light'>{certificate.duration}</span>
                         </div>
-                        <div className=' justify-between flex absolute bottom-[9px] left-[9px] right-[9px] xss:*:*:!leading-[0.68] *:*:!leading-[0.8]   flex p-[11px] bg-[#F5F5F580] rounded-[4px] border border-[#000] backdrop-blur-[4px]'>
-                            <div className="flex flex-col gap-[10px]">
-                                <span className='text-5'>Срок оформления</span>
-                                <span className='text-1 font-light'>{certificate.duration}</span>
-                            </div>
-                            <div className="flex flex-col gap-[10px]">
-                                <span className='text-5'>Стоимость</span>
-                                <span className='text-1 font-light'>{certificate.price}</span>
-                            </div>
+                        <div className="flex flex-col gap-[10px]">
+                            <span className='text-5'>Стоимость</span>
+                            <span className='text-1 font-light'>{certificate.price}</span>
                         </div>
                     </div>
                 </div>
+            </div>
         )
     );
 };
