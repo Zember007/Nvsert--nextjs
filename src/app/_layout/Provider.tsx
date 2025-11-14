c6daf9a01118ac46148f8ec4e77c89d19252d30e
Georgii Borisov <goraegorgora@mail.ru>
2025-11-14 15:05:40 +0400

    fix

--- src/app/_layout/Provider.tsx
+++ src/app/_layout/Provider.tsx
@@ -1,7 +1,10 @@
 
 
+"use client";
 import '@/config/i18n';
+import { Provider } from 'react-redux';
 import { HeaderContextProvider } from '@/components/contexts/HeaderContext';
+import { store } from '@/config/store'
 import Layout_wrapper from './Layout_wrapper';
 import { ReactNode } from 'react';
 import { TypographyProvider } from './Typography/TypographyProvider';
@@ -10,21 +13,20 @@ import { NavigationItem } from '@/store/navigation';
 
 function ReduxProvider({ children, initialNavigation }: { children: ReactNode; initialNavigation?: NavigationItem[] }) {
 
-  return (
-    <TypographyProvider
-      threshold={16}
-      smallWeightMac={300}
-      largeWeightMac={400}
-      smallWeightWindows={350}
-      largeWeightWindows={400}
-    >
-      <HeaderContextProvider initialNavigation={initialNavigation}>
-        <Layout_wrapper initialNavigation={initialNavigation}>
-          {children}
-        </Layout_wrapper>
-      </HeaderContextProvider>
-    </TypographyProvider>
-  )
+  return <TypographyProvider
+    threshold={16}
+    smallWeightMac={300}
+    largeWeightMac={400}
+    smallWeightWindows={350}
+    largeWeightWindows={400}
+  >
+    <HeaderContextProvider initialNavigation={initialNavigation}>
+      <Layout_wrapper initialNavigation={initialNavigation}>
+        {children}
+      </Layout_wrapper>
+    </HeaderContextProvider>
+  </TypographyProvider>
+  
 }
 
 export default ReduxProvider
