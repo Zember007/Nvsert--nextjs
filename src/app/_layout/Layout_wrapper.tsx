da63130446447faa8f8cbf50ff7e2bb3a5530918
Georgii Borisov <goraegorgora@mail.ru>
2025-11-14 15:58:53 +0400

    fix

--- src/app/_layout/Layout_wrapper.tsx
+++ src/app/_layout/Layout_wrapper.tsx
@@ -7,56 +7,13 @@ import AppFooter from '@/components/general/AppFooter';
 import { useHeaderContext } from '@/components/contexts/HeaderContext';
 import AppModalWrapper from '@/components/general/AppModalWrapper';
 import CopyNotification from '@/components/general/elements/CopyNotification';
-import { NavigationItem, Services } from '@/store/navigation';
+import { NavigationItem } from '@/store/navigation';
 import { groupServices } from '@/assets/lib/navigation';
 
 
 const LayoutContent = ({ children, initialNavigation }: { children: ReactNode; initialNavigation?: NavigationItem[]; }) => {
 
     const { setDefaultModalActive, defaultModalActive, defaultModalName, resetCountModal, defaultModalCount, showCopyNotification, notificationPosition, hideNotification } = useHeaderContext();
-
-
-    /* 
-        const configs = useMemo(() => {
-            if (!configsPure) return {};
-            
-            const parsedConf: any = {};
-            configsPure.forEach((item) => {
-                parsedConf[item.key] = item.value;
-            });
-            return parsedConf;
-        }, [configsPure]); */
-    /* 
-        const file_configs = useMemo(() => {
-            if (!fileConfigsPure) return {};
-            
-            const parsedConf: any = {};
-            fileConfigsPure.forEach((item) => {
-                parsedConf[item.key] = item.value;
-            });
-            return parsedConf;
-        }, [fileConfigsPure]); */
-
-    /*     useEffect(() => {
-            if (Object.keys(configs).length > 0 && Object.keys(file_configs).length > 0) {
-                dispatch(setMetadata(generateMetadata(configs, file_configs)));
-            }
-        }, [configs, file_configs, dispatch]); */
-
-    // Инициализируем навигацию из серверных данных синхронно до первого рендера
-
-
-    // Загружаем данные только один раз при монтировании, если они еще не загружены
-    /*   useEffect(() => {
-          if (status === 'idle') {
-              // dispatch(updateActionConfigs());
-              // dispatch(updateActionFileConfigs());
-              // Загружаем навигацию только если она еще не была инициализирована
-              if (navigationStatus === 'idle' && navigation.length === 0 && !initialNavigation) {
-                  dispatch(updateActionNavigation());
-              }
-          }
-      }, [dispatch, status, navigationStatus, navigation.length, initialNavigation]); */
 
 
 
--- src/app/_layout/Provider.tsx
+++ src/app/_layout/Provider.tsx
@@ -5,25 +5,17 @@ import '@/config/i18n';
 import { HeaderContextProvider } from '@/components/contexts/HeaderContext';
 import Layout_wrapper from './Layout_wrapper';
 import { ReactNode } from 'react';
-import { TypographyProvider } from './Typography/TypographyProvider';
 import { NavigationItem } from '@/store/navigation';
 
 
 function ReduxProvider({ children, initialNavigation }: { children: ReactNode; initialNavigation?: NavigationItem[] }) {
 
-  return <TypographyProvider
-    threshold={16}
-    smallWeightMac={300}
-    largeWeightMac={400}
-    smallWeightWindows={350}
-    largeWeightWindows={400}
-  >
-    <HeaderContextProvider initialNavigation={initialNavigation}>
+  return <HeaderContextProvider initialNavigation={initialNavigation}>
       <Layout_wrapper initialNavigation={initialNavigation}>
         {children}
       </Layout_wrapper>
     </HeaderContextProvider>
-  </TypographyProvider>
+
 }
 
 export default ReduxProvider
