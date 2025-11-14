76e379f8f7e99a090e08bf900246e70f001ebe95
Georgii Borisov <goraegorgora@mail.ru>
2025-11-14 12:36:01 +0400

    fix

--- src/components/main/AppMainContent.tsx
+++ src/components/main/AppMainContent.tsx
@@ -1,12 +1,24 @@
-import AppMainDocuments from './AppMainDocuments';
-import AppMainSkills from './AppMainSkills';
-import AppMainSlider from './AppMainSlider';
-import AppMainSafeguards from './AppMainSafeguards';
+'use client';
+
+import dynamic from 'next/dynamic';
 
 
 export default function AppMainContent() {
+  const AppMainDocuments = dynamic(() => import('./AppMainDocuments'), {
+    ssr: false
+  })
+  const AppMainSkills = dynamic(() => import('./AppMainSkills'), {
+    ssr: false
+  })
+  const AppMainSlider = dynamic(() => import('./AppMainSlider'), {
+    ssr: false
+  })
+  const AppMainSafeguards = dynamic(() => import('./AppMainSafeguards'), {
+    ssr: false
+  })
   return (
     <>
+    
       <AppMainDocuments />
       <AppMainSkills />
       <AppMainSlider />
