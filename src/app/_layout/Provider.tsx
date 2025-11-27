c43aa36b5f6260735e44bc5b69c4688c98004f43
Georgii Borisov <goraegorgora@mail.ru>
2025-11-27 14:09:55 +0400

    fix

--- src/app/_layout/Provider.tsx
+++ src/app/_layout/Provider.tsx
@@ -11,19 +11,21 @@ import { NavigationItem } from '@/store/navigation';
 
 function ReduxProvider({ children, initialNavigation }: { children: ReactNode; initialNavigation: NavigationItem[] }) {
 
-  return <TypographyProvider
+  return <HeaderContextProvider initialNavigation={initialNavigation}>
+      <Layout_wrapper initialNavigation={initialNavigation}>
+        {children}
+      </Layout_wrapper>
+    </HeaderContextProvider>
+
+
+{/* <TypographyProvider
     threshold={16}
     smallWeightMac={300}
     largeWeightMac={400}
     smallWeightWindows={350}
     largeWeightWindows={400}
-  >
-    <HeaderContextProvider initialNavigation={initialNavigation}>
-      <Layout_wrapper initialNavigation={initialNavigation}>
-        {children}
-      </Layout_wrapper>
-    </HeaderContextProvider>
-  </TypographyProvider>
+  > */}
+    
 }
 
 export default ReduxProvider
