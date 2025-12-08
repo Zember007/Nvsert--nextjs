c53940c34f712e0ec19af5518fdf90ee066db5f1
Georgii Borisov <goraegorgora@mail.ru>
2025-12-08 14:21:07 +0400

    fix

--- src/components/common/AsyncPhotoView.tsx
+++ src/components/common/AsyncPhotoView.tsx
@@ -5,22 +5,21 @@ import dynamic from 'next/dynamic';
 import React, { useEffect } from 'react';
 import type { ComponentProps } from 'react';
 
-// Глобальное состояние, чтобы стили подключались только один раз
-let photoViewStylesLoaded = false;
 
 const ensurePhotoViewStylesLoaded = () => {
-  if (photoViewStylesLoaded) return;
-  photoViewStylesLoaded = true;
+  if (!window || (window as any).photoViewStylesLoaded) return;
+  (window as any).photoViewStylesLoaded = true;
 
   // Динамически подключаем стили только при первом использовании
-  void import('@/assets/lib/react-photo-view/dist/react-photo-view.css');
-  void import('@/assets/styles/blocks/photoviews.scss');
+  void import('@/assets/lib/react-photo-view/dist/react-photo-view.css').then(() => {
+    void import('@/assets/styles/blocks/photoviews.scss');
+  });
 };
 
 type AnyProps = ComponentProps<any>;
 
 // Базовые лениво загружаемые компоненты
-/* const DynamicPhotoProvider = dynamic(
+const DynamicPhotoProvider = dynamic(
   () => import('@/assets/lib/react-photo-view').then((m) => m.PhotoProvider),
   {
     ssr: false,
@@ -32,23 +31,22 @@ const DynamicPhotoView = dynamic(
   {
     ssr: false,
   }
-) as React.ComponentType<AnyProps>; */
+) as React.ComponentType<AnyProps>;
 
 // Обёртки, которые один раз инициализируют стили
 export const AsyncPhotoProvider: React.FC<AnyProps> = (props) => {
-/*   useEffect(() => {
+ /*  useEffect(() => {
     ensurePhotoViewStylesLoaded();
   }, []); */
 
-  return <>
-  {props.children}</>;
+  return <DynamicPhotoProvider {...props} />;
 };
 
 export const AsyncPhotoView: React.FC<AnyProps> = (props) => {
-/*   useEffect(() => {
+ /*  useEffect(() => {
     ensurePhotoViewStylesLoaded();
   }, []); */
 
-  return <>{props.children}</>;
+  return <DynamicPhotoView {...props} />;
 };
 
