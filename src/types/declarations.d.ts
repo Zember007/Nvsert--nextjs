0387de6208c8152362f29dce2b83f34066a059fd
Georgii Borisov <goraegorgora1306@gmail.com>
2026-01-30 18:05:45 +0400

    edit i18n

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -1,4 +1,24 @@
 declare module '*.mp4' {
     const src: string;
     export default src;
-  }
+}
+
+declare module '*.jpg' {
+  const content: import('next/image').StaticImageData;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: import('next/image').StaticImageData;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: import('next/image').StaticImageData;
+  export default content;
+}
+
+declare module '*.webp' {
+  const content: import('next/image').StaticImageData;
+  export default content;
+}
--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -1,12 +1,17 @@
 // src/types/svg.d.ts
 declare module '*.svg' {
     import * as React from 'react';
+    import { StaticImageData } from 'next/image';
   
     const ReactComponent: React.FunctionComponent<
       React.SVGProps<SVGSVGElement> & { title?: string }
     >;
   
-    export default ReactComponent;
+    // Support for using SVG as image source (StaticImageData compatible)
+    const content: StaticImageData;
+  
+    export default content;
+    export { ReactComponent };
   }
   
   declare module '*.svg?react' {
@@ -17,4 +22,9 @@ declare module '*.svg' {
     >;
   
     export default ReactComponent;
+  }
+
+  declare module '*.svg?url' {
+    const content: string;
+    export default content;
   }
