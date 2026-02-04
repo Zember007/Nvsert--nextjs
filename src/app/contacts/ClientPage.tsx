2b8c4a823d2f094515a07f87d6f3cb76d66f3820
Georgii Borisov <goraegorgora1306@gmail.com>
2026-02-04 11:08:03 +0400

    refactoring && edit

--- src/app/contacts/ClientPage.tsx
+++ src/app/contacts/ClientPage.tsx
@@ -288,11 +288,47 @@ const ClientPage = ({ data }: { data: ContactsPageData }) => {
             </div>
 
             <div className="flex m:gap-[26px] gap-[20px] xl:flex-row flex-col">
-              {(data.connectSection.featureCards || []).map((card) => (
+              {(data.connectSection.featureCards || []).map((card, index) => (
                 <div
                   key={card.title}
-                  className="relative p-[40px] bg-[#f1f1ed] w-full rounded-[8px] border border-[#93969d] flex flex-col gap-[20px]"
+                  className="relative p-[40px] bg-[#93969d26] w-full rounded-[8px] border border-[#93969d] flex flex-col gap-[20px]"
                 >
+                  <div className="absolute top-[10px] right-[10px]">
+                    {index === 0 &&
+                      <svg width="66" height="45" viewBox="0 0 66 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M23.6613 0L0 22.5L20.9099 42.3836L26.4128 37.1509L11.0057 22.5L23.6613 10.4655L36.317 22.5L33 25.6542L26.9316 19.8836L21.4287 25.1164L42.3387 45L66 22.5L42.3387 0L33 8.88029L23.6613 0ZM38.5029 14.1131L47.3227 22.5L38.5028 30.8869L42.3387 34.5345L54.9943 22.5L42.3387 10.4655L38.5029 14.1131Z" fill="#34446D" fill-opacity="0.2" />
+                      </svg>
+                    }
+                    {index === 1 &&
+                      <svg width="35" height="45" viewBox="0 0 35 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g opacity="0.2">
+                          <path d="M22.4992 25.1992L34.9987 32.3992L13.1245 44.9992L0 37.7992L22.4992 25.1992Z" fill="url(#paint0_linear_15508_15332)" />
+                          <path d="M5.78279e-07 12.5996L13.1245 19.7996V44.9996L0 37.7996L5.78279e-07 12.5996Z" fill="#3C4D79" />
+                          <path d="M22.5 0L34.9995 7.2V32.4L22.5 25.2V0Z" fill="url(#paint1_linear_15508_15332)" />
+                          <path d="M22.4991 0L34.9987 7.2L13.1245 19.8L0 12.6L22.4991 0Z" fill="#3B4D78" />
+                        </g>
+                        <defs>
+                          <linearGradient id="paint0_linear_15508_15332" x1="28.1239" y1="29.0992" x2="8.6397" y2="42.7351" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#0A1E53" />
+                            <stop offset="0.375754" stop-color="#34446D" />
+                          </linearGradient>
+                          <linearGradient id="paint1_linear_15508_15332" x1="28.7498" y1="0" x2="28.7498" y2="32.4" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#00103A" />
+                            <stop offset="0.531505" stop-color="#34446D" />
+                          </linearGradient>
+                        </defs>
+                      </svg>
+
+                    }
+                    {index === 2 &&
+                      <svg width="45" height="45" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2.8125 22.5V42.1875H22.5M22.5 2.8125H42.1875V22.5" stroke="#34446D" stroke-opacity="0.2" stroke-width="5.625" />
+                        <path d="M8.4375 22.5L8.4375 8.4375H22.5M22.5 36.5625H36.5625V22.5" stroke="#34446D" stroke-opacity="0.2" stroke-width="5.625" />
+                        <rect x="11.25" y="33.75" width="11.25" height="11.25" transform="rotate(-90 11.25 33.75)" fill="#34446D" fill-opacity="0.2" />
+                        <rect x="22.5" y="22.5" width="11.25" height="11.25" transform="rotate(-90 22.5 22.5)" fill="#34446D" fill-opacity="0.2" />
+                      </svg>
+                    }
+                  </div>
                   <span className={textSize.headerH6}>{card.title}</span>
                   <p className={textSize.textBasePost}>{card.text}</p>
                 </div>
