8ca7af5bc84343793eaa2f2eea6e08d6690af844
Georgii Borisov <goraegorgora1306@gmail.com>
2026-02-19 17:11:23 +0400

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -9,29 +9,32 @@ const nextConfig = {
   // Убираем ошибку Turbopack + webpack
   turbopack: {}, // Это официально рекомендованный способ заглушить ошибку
   images: {
+    unoptimized: false,
+    dangerouslyAllowSVG: true,
     remotePatterns: [
-      
+
       {
         protocol: 'https',
         hostname: 'test11-admin.audiosector.ru',
         port: '',
         pathname: '/uploads/**',
       },
-        {
-          protocol: 'https',
-          hostname: 'test11.audiosector.ru',
-          pathname: '/uploads/**',
-        },
+      {
+        protocol: 'https',
+        hostname: 'test11.audiosector.ru',
+        pathname: '/uploads/**',
+      },
     ],
-  
+
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 192, 256, 384],
     minimumCacheTTL: 86400,
-    dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-  
+  experimental: {
+    allowedRevalidateHeaderKeys: [],
+  },
 
   reactStrictMode: true,
   compress: true,
