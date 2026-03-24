296d33ccccc87e7e2d62e4a33a86860d584f244a
Georgii Borisov <goraegorgora1306@gmail.com>
2026-03-24 15:00:01 +0400

    edit

--- src/widgets/services/ServiceDetailLayout.tsx
+++ src/widgets/services/ServiceDetailLayout.tsx
@@ -13,12 +13,12 @@ import { useTranslation } from 'react-i18next';
 
 
 const ServiceRecommendedList = dynamic(
-  () => import('widgets/services').then((m) => m.ServiceRecommendedList),
-  { ssr: false },
+    () => import('widgets/services').then((m) => m.ServiceRecommendedList),
+    { ssr: false },
 );
 
 const DotNavList = dynamic(() => import('widgets/layout').then((m) => m.DotNavList), {
-  ssr: false,
+    ssr: false,
 });
 
 interface ServiceDetailLayoutProps {
@@ -66,132 +66,133 @@ const ServiceDetailLayout: React.FC<ServiceDetailLayoutProps> = ({
         return () => window.clearTimeout(timeoutId);
     }, []);
     const twoColumnContent = (
-                    <div className="flex gap-[40px] items-stretch m:flex-row flex-col">
-                        {/* Left Column */}
-                        <div className="m:w-[265px] relative">
-                            <div className="sticky top-[104px] flex flex-col xl:gap-[40px] gap-[20px] no-scrollbar m:overflow-y-auto m:max-h-[calc(100vh-104px)]">
-                                <div className="flex  gap-[20px] flex-col-reverse">
-                                    <div className="w-[250px] mx-auto">
-                                        <ServiceCard
-                                            onClick={() => {console.log(document.getElementById('service-' + index));
-                                                document.getElementById('service-' + index)?.click();
-                                            }}
-                                            serviceName={currentService?.category?.name || ''}
-                                            certificate={{
-                                                ...currentService,
-                                                slug: '',
-                                                id: currentService?.id || 0,
-                                                documentId: currentService?.documentId || '',
-                                                title: currentService?.title || '',
-                                                duration: currentService?.duration || '',
-                                                price: currentService?.price || '',
-                                                description: currentService?.description || '',
-                                                createdAt: currentService?.createdAt || '',
-                                                updatedAt: currentService?.updatedAt || '',
-                                                publishedAt: currentService?.publishedAt || '',
-                                                documents: currentService?.documents || [],
-                                                img: currentService?.img || null,
-                                                category: currentService?.category || null,
-                                            }}
-                                            title={false}
-                                            padding={false}
-                                            priority={true}
-                                        />
-                                    </div>
-
-                                    <Button
-                                        wrapperClassName="xl:hidden block !max-w-[250px]"
-                                        onClick={onOpenOrderForm}
-                                        label={t('form.buttons.submitApplication')}
-                                    />
-                                </div>
-
-                                {showSecondaryUI && (
-                                    <ServiceRecommendedList
-                                        items={recomendedServices}
-                                        wrapperClassName="w-full m:block hidden"
-                                        itemClassName={undefined}
-                                        textClassName="m:!whitespace-pre-line !whitespace-normal"
-                                    />
-                                )}
-                            </div>
+        <div className="flex gap-[40px] items-stretch m:flex-row flex-col">
+            {/* Left Column */}
+            <div className="m:w-[265px] relative">
+                <div className="sticky top-[104px] flex flex-col xl:gap-[40px] gap-[20px] no-scrollbar m:overflow-y-auto m:max-h-[calc(100vh-104px)]">
+                    <div className="flex  gap-[20px] flex-col-reverse">
+                        <div className="w-[250px] mx-auto">
+                            <ServiceCard
+                                onClick={() => {
+                                    console.log(document.getElementById('service-' + index));
+                                    document.getElementById('service-' + index)?.click();
+                                }}
+                                serviceName={currentService?.category?.name || ''}
+                                certificate={{
+                                    ...currentService,
+                                    slug: '',
+                                    id: currentService?.id || 0,
+                                    documentId: currentService?.documentId || '',
+                                    title: currentService?.title || '',
+                                    duration: currentService?.duration || '',
+                                    price: currentService?.price || '',
+                                    description: currentService?.description || '',
+                                    createdAt: currentService?.createdAt || '',
+                                    updatedAt: currentService?.updatedAt || '',
+                                    publishedAt: currentService?.publishedAt || '',
+                                    documents: currentService?.documents || [],
+                                    img: currentService?.img || null,
+                                    category: currentService?.category || null,
+                                }}
+                                title={false}
+                                padding={false}
+                                priority={true}
+                            />
                         </div>
 
-                        {/* Right Column */}
-                        <div className="flex-1 flex flex-col items-center gap-[50px]">
-                            {/* Dynamic Content Blocks */}
-                            <div className="xl:hidden block w-full">
-                                {showSecondaryUI && (
-                                    <DotNavList
-                                        position={null}
-                                        items={currentService?.content?.map((item, index) => ({
-                                            id: index,
-                                            title: item.heading,
-                                            index: index,
-                                        }))}
-                                    />
-                                )}
-                            </div>
-                            <div className="w-full flex flex-col items-center space-y-[50px]">
-                                {currentService.content?.map((block, index) => (
-                                    <React.Fragment key={block.id}>
-                                        {index === ctaInsertAfterIndex && (
-                                            <AppCtaBanner
-                                                key="cta-banner"
-                                                text={currentService?.cta?.text || ''}
-                                                description={currentService?.cta?.description || ''}
-                                                onButtonClick={onOpenIntroForm}
-                                            />
-                                        )}
-                                        <ServiceContentBlock
-                                            index={index}
-                                            block={block}
-                                            isExpanded={!expandedSections.includes(block.id)}
-                                            onToggle={() => onToggleSection(block.id)}
-                                            isFirst={index === 0}
-                                        />
-                                    </React.Fragment>
-                                ))}
-                            </div>
-                            {currentService.content && currentService.content.length > 0 && (
-                                <ServiceArticleDate
-                                    date={currentService.createdAt}
-                                />
-                            )}
-                        </div>
-
-                        <div className="w-full m:hidden block">
-                            {showSecondaryUI && (
-                                <ServiceRecommendedList
-                                    items={recomendedServices}
-                                    wrapperClassName="w-full m:hidden block"
-                                    itemClassName="w-full"
-                                    textClassName="m:!whitespace-pre-line !whitespace-normal"
-                                />
-                            )}
-                        </div>
-                    </div>
-    );
-
-    const sidebar = (
-                <div className="w-[250px] relative xl:block hidden">
-                    <div className=" sticky top-[104px] flex flex-col gap-[50px] no-scrollbar overflow-y-auto max-h-[calc(100vh-104px)]">
                         <Button
+                            wrapperClassName="xl:hidden block !max-w-[250px]"
                             onClick={onOpenOrderForm}
                             label={t('form.buttons.submitApplication')}
                         />
-
-                        {showSecondaryUI && (
-                            <DotNavList
-                                items={currentService?.content?.map((item, index) => ({
-                                    id: index,
-                                    title: item.heading,
-                                    index: index,
-                                }))}
-                            />
-                        )}
                     </div>
+
+                    {showSecondaryUI && (
+                        <ServiceRecommendedList
+                            items={recomendedServices}
+                            wrapperClassName="w-full m:block hidden"
+                            itemClassName={undefined}
+                            textClassName="m:!whitespace-pre-line !whitespace-normal"
+                        />
+                    )}
                 </div>
+            </div>
+
+            {/* Right Column */}
+            <div className="flex-1 flex flex-col items-center gap-[50px]">
+                {/* Dynamic Content Blocks */}
+                <div className="xl:hidden block w-full">
+                    {showSecondaryUI && (
+                        <DotNavList
+                            position={null}
+                            items={currentService?.content?.map((item, index) => ({
+                                id: index,
+                                title: item.heading,
+                                index: index,
+                            }))}
+                        />
+                    )}
+                </div>
+                <div className="w-full flex flex-col items-center space-y-[50px]">
+                    {currentService.content?.map((block, index) => (
+                        <React.Fragment key={block.id}>
+                            {index === ctaInsertAfterIndex && (
+                                <AppCtaBanner
+                                    key="cta-banner"
+                                    text={currentService?.cta?.text || ''}
+                                    description={currentService?.cta?.description || ''}
+                                    onButtonClick={onOpenIntroForm}
+                                />
+                            )}
+                            <ServiceContentBlock
+                                index={index}
+                                block={block}
+                                isExpanded={!expandedSections.includes(block.id)}
+                                onToggle={() => onToggleSection(block.id)}
+                                isFirst={index === 0}
+                            />
+                        </React.Fragment>
+                    ))}
+                </div>
+                {currentService.content && currentService.content.length > 0 && (
+                    <ServiceArticleDate
+                        date={currentService.createdAt}
+                    />
+                )}
+            </div>
+
+            <div className="w-full m:hidden block">
+                {showSecondaryUI && (
+                    <ServiceRecommendedList
+                        items={recomendedServices}
+                        wrapperClassName="w-full m:hidden block"
+                        itemClassName="w-full"
+                        textClassName="m:!whitespace-pre-line !whitespace-normal"
+                    />
+                )}
+            </div>
+        </div>
+    );
+
+    const sidebar = (
+        <div className="w-[250px] relative xl:block hidden">
+            <div className=" sticky top-[104px] flex flex-col gap-[50px] no-scrollbar overflow-y-auto max-h-[calc(100vh-104px)]">
+                <Button
+                    onClick={onOpenOrderForm}
+                    label={t('form.buttons.submitApplication')}
+                />
+
+                {showSecondaryUI && (
+                    <DotNavList
+                        items={currentService?.content?.map((item, index) => ({
+                            id: index,
+                            title: item.heading,
+                            index: index,
+                        }))}
+                    />
+                )}
+            </div>
+        </div>
     );
 
     if (contentOnly) {
